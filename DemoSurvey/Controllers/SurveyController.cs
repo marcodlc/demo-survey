8ce807badd46c2859347fb5107bb93865ccbc34b
marco.delacruz@nagnoi.com <marco.delacruz@nagnoi.com>
2024-11-04 08:40:29 -0500

    Endpoint demo

---
+++ DemoSurvey/Controllers/SurveyController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoSurvey.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SurveyController : ControllerBase
+    {
+        private readonly ILogger<SurveyController> _logger;
+
+        public SurveyController(ILogger<SurveyController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost(Name = "answer")]
+        public IActionResult SurveyReply([FromBody] dynamic answer)
+        {
+            try
+            {
+                // Procesa la respuesta de la encuesta aquí (por ejemplo, guarda en la base de datos)
+                Console.WriteLine($"Respuesta recibida: ${answer}");
+                return Ok(new { mensaje = "Answer received successfully" });
+            }
+            catch (Exception ex) {
+                Console.WriteLine($"Error: ${ex.Message}");
+                return BadRequest("answer is null");
+            }
+        }
+    }
+}
